bd87e1ec9e10dfc9d98c01b33a1af5e9533fbb5a
Dc-coders <fd9577110@gmail.com>
2024-06-29 14:02:50 +0500

    init Commit

---
+++ index.js
@@ -0,0 +1,50 @@
+
+
+// . => ichidagi 
+// ; => kodlash tamomlandi
+// , => ko'dlash davom etadi
+// + qo'shish
+//- ayirish
+//* ko'paytirish
+// / bo'lish
+
+// ++ bittaga oshirish
+// -- bittaga kamaytishish
+// ** draja olish
+// % qoldiq olish
+
+
+
+
+
+
+
+
+//1 Number
+//2 String
+//3 NaN   => Not a Number
+//4 Bigint
+//5 Null
+//6 Undefinet
+//7 Boolean
+//8 Object
+
+//Taqqoslash operatorlari;
+
+//1 ==
+//2 ===
+//3 >
+//4 <
+//5 >=
+//6 <=
+//7 => &&
+//8 || => Or
+//9 !  => Not
+
+
+            
+
+
+
+
+
